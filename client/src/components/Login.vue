66f401d27703df4b6cc5dd7391aadab22c06daac
Mitch <jamlets@protonmail.com>
2023-06-22 07:21:58 -0400

    login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,17 +1,60 @@
 <template>
-  <div >
-    <h1>Login Module</h1>
-    <p>METAMASK/WALLET CONNECT</p>
-    <p>ACCOUNT BALANCE/LOGOUT</p>
-  </div>
-</template>
-
-<script>
-export default {
-    name: "Login"
-}
-</script>
-
-<style>
-
-</style>
+    <div>
+      <h1>Login Module</h1>
+      <p v-if="!isAuthenticated">Please connect with Metamask</p>
+      <button v-if="!isAuthenticated" @click="metaLogin">Connect Metamask</button>
+      <div v-else>
+          <p>Connected with Metamask</p>
+          <p>Account: {{ address.slice(0, 4) }}...{{ address.slice(address.length - 4) }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { useStore } from 'vuex';
+  import { computed, onMounted } from 'vue';
+  import { ethers } from "ethers";
+  import { handleLogin, handleCurrentUser, handleAccountChange, setupNetworkChangeListener, setupAccountChangeListener } from '../api/ethersConnect';
+  
+  export default {
+      name: "Login",
+      setup() {
+          const store = useStore()
+  
+          const metaLogin = async () => {
+              await handleLogin(store);
+          }
+  
+          window.ethereum.on('accountsChanged', (accounts) => {
+              handleAccountChange(store, accounts);
+          });
+          
+          const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+          provider.on("network", (newNetwork, oldNetwork) => {
+              if (oldNetwork) {
+                  window.location.reload();
+              }
+          });
+  
+          onMounted(() => {
+              handleCurrentUser(store);
+              setupNetworkChangeListener();
+              setupAccountChangeListener();
+          })
+  
+          return {
+              isAuthenticated: computed(() => Object.keys(store.state.user).length > 0),
+              address: computed(() => store.state.address),
+              metaLogin,
+          }
+      },
+      created() {
+          handleCurrentUser(this.$store);
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
